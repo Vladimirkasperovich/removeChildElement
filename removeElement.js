03f957df8268945bf7363853f15e5172cb31da06
Vladimirkasperovich <vova.kasper.boy@gmail.com>
2022-08-21 08:05:30 +0300

    first commit
    first commit

---
+++ removeElement.js
@@ -0,0 +1,20 @@
+//Сделайте так, чтобы любая li удалялась по клику на нее.
+/*let elems = document.querySelectorAll('li');
+
+for (let elem of elems){
+    elem.addEventListener('click', function(){
+        elem.remove();
+    });
+
+}*/ 
+
+
+//Сделайте так, чтобы по каждому клику на кнопку удалялся последний элемент из #parent.
+/*let parent = document.getElementById('parent');
+let button = document.querySelector('#button');
+button.addEventListener('click', function(){
+    parent.removeChild(parent.lastElementChild);
+
+  
+
+});*/
